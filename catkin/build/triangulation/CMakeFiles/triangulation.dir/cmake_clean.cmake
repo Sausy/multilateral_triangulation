b423f5b1664a50aeb971f19c80aea1c523b2fc4f
Sausy <Michael.Sausmikat@gmx.at>
2019-04-11 17:27:53 +0200

    added catkin workspace for ros

---
+++ catkin/build/triangulation/CMakeFiles/triangulation.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/triangulation.dir/src/fpga_interface.cpp.o"
+  "CMakeFiles/triangulation.dir/src/main.cpp.o"
+  "CMakeFiles/triangulation.dir/src/peripherals.cpp.o"
+  "CMakeFiles/triangulation.dir/src/piezo_ctl.cpp.o"
+  "CMakeFiles/triangulation.dir/src/time_controll.cpp.o"
+  "CMakeFiles/triangulation.dir/src/time_sync.cpp.o"
+  "CMakeFiles/triangulation.dir/src/trap.cpp.o"
+  "/home/sausy/Projects/multilateral_triangulation/catkin/devel/lib/triangulation/triangulation.pdb"
+  "/home/sausy/Projects/multilateral_triangulation/catkin/devel/lib/triangulation/triangulation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/triangulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin/src/triangulation/lib/time_sync.hpp
@@ -0,0 +1,33 @@
+#ifndef TIME_SYNC_HPP_
+#define TIME_SYNC_HPP_
+#include <ros/ros.h>
+#include <vector>
+
+/*
+struct time {
+    vector<double>    sync_time_div;
+    vector<double>    time_div;
+    vector<double>    sys_time;
+};
+
+class time_sync
+{
+  public:
+    time_sync(int32_t *base_addr_ = nullptr);
+
+    ros::NodeHandle   nh;
+    ros::Subscriber   time_ctl_sub,   module_mode_sub;
+    ros::Publisher    time_pub;
+
+    //subs
+    void get_current_mode(const triangulation_msg::mode_msg::ConstPtr& msg);
+    void start_sync_mode();
+
+    //pubs
+    void update_time(time *time_data);
+
+  private:
+    int32_t *rtc_base;
+};
+*/
+#endif
